d8a0a5c1956cdabb1830e843d1bc868e59dded38
Renaud <renaud.wangler@ib.cegos.fr>
2026-07-30 17:10:31 +0200

    auto nav

--- hooks/navigation.py
+++ hooks/navigation.py
@@ -1,6 +1,9 @@
-# hooks/navigation.py
+import re
+from pathlib import Path
 
 def on_page_markdown(markdown, page, config, files):
-    print("HOOK MKDOCS")
-    markdown += "\n\nHOOK_MKDOCS\n"
+    fichier = Path(page.file.src_uri).name
+    if re.fullmatch(r"a\d+e\d+\.md", fichier, re.IGNORECASE):
+        print(f"Injection navigation : {fichier}")
+        markdown += "\n\n{{ ibNav() }}\n"
     return markdown
