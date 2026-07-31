79a5a12a53d26df726869db206f3631fe82f1d48
Renaud <renaud.wangler@ib.cegos.fr>
2026-07-31 12:26:29 +0200

    bouton copie avec feedback

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -3,3 +3,12 @@ function ibCopy(texte) {
     navigator.clipboard.writeText(texte);
     console.log(texte);
 }
+
+async function ibCopy(text, button) {
+    await navigator.clipboard.writeText(text);
+    button.classList.add("ok");
+    setTimeout(
+        () => button.classList.remove("ok"),
+        1500
+    );
+}
