b37a05c48fdcdc49201805b174d90d06a514bee1
Renaud <renaud.wangler@ib.cegos.fr>
2026-07-31 15:57:24 +0200

    settings v1

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -14,8 +14,15 @@ document.addEventListener(
         tasks.forEach(task => { if ( localStorage.getItem(task.id) === "true" ) { task.classList.add("done"); }});
     /* Gestion du clic */
         tasks.forEach((task, index) => { task.addEventListener("click", event => {
-        if ( event.clientX > (task.getBoundingClientRect()).left + (parseFloat( getComputedStyle(task).fontSize ) * 4) ) { return; }
+        if ( event.clientX > (task.getBoundingClientRect()).left + (parseFloat( getComputedStyle(task).fontSize ) * 3) ) { return; }
         if (task.classList.contains("done")) {
             for ( let i = index; i < tasks.length; i++) { tasks[i].classList.remove("done"); localStorage.removeItem(tasks[i].id);}}
         else {
             for ( let i = 0; i <= index; i++) { tasks[i].classList.add("done"); localStorage.setItem( tasks[i].id,"true");}}});})});
+
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+        const panel = document.getElementById( "ibSettingsPanel" );
+        document.getElementById( "ibSettingsButton" ).addEventListener( "click", () => panel.classList.toggle( "open" ));
+        document.getElementById( "ibSettingsClose"  ).addEventListener( "click", () => panel.classList.remove( "open" ));});            
