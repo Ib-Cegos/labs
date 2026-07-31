9ab3ccd747aa7c49bd228e3b340c3d648bfc9636
Renaud <renaud.wangler@ib.cegos.fr>
2026-07-31 15:45:38 +0200

    test marqueurs cliquables

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -6,9 +6,16 @@ async function ibCopy(text, button) {
     button.classList.add("ok");
     setTimeout( () => button.classList.remove("ok"), 1500 );}
 
-document.addEventListener( "DOMContentLoaded", () => {
-        document
-            .querySelectorAll(".ibLabTask")
-            .forEach(task => {
-                task.addEventListener( "click", () => { task.classList.toggle("done"); });
-            });});
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+        const tasks = Array.from( document.querySelectorAll(".ibLabTask") );
+    /* Restauration de l'état */
+        tasks.forEach(task => { if ( localStorage.getItem(task.id) === "true" ) { task.classList.add("done"); }});
+    /* Gestion du clic */
+        tasks.forEach((task, index) => { task.addEventListener("click", event => {
+        if ( event.clientX > (task.getBoundingClientRect()).left + (parseFloat( getComputedStyle(task).fontSize ) * 4) ) { return; }
+        if (task.classList.contains("done")) {
+            for ( let i = index; i < tasks.length; i++) { tasks[i].classList.remove("done"); localStorage.removeItem(tasks[i].id);}}
+        else {
+            for ( let i = 0; i <= index; i++) { tasks[i].classList.add("done"); localStorage.setItem( tasks[i].id,"true");}}});})});
