a73baa581b13723ad43f08864e19a2fcc313eaf7
Renaud <renaud.wangler@ib.cegos.fr>
2026-08-05 18:38:03 +0200

    bouton Notes v1

--- hooks/content_processor.py
+++ hooks/content_processor.py
@@ -113,11 +113,14 @@ def injecter_variables(html, page):
     code_atelier = (Path(page.file.src_uri).parent.name.lower())
     fichier = Path(page.file.src_uri).name
     est_exercice = bool( re.fullmatch(r"a\d+e\d+\.md", fichier, re.IGNORECASE ))
+    code_exercice = None
+    if est_exercice: code_exercice = Path(page.file.src_uri).stem.lower()
     script = (
     "<script>"
     f"window.ibLabCode = {json.dumps(code_atelier)};"
     f"window.ibVariables = {json.dumps(variables, ensure_ascii=False)};"
     f"window.ibIsExercise = {str(est_exercice).lower()};"
+    f"window.ibExerciseCode = {json.dumps(code_exercice)};"
     "</script>" )
     return script + html
 
