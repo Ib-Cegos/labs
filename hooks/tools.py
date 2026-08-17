a6479dcca226efdc22614ef12cc637537ef34c7e
Renaud <renaud.wangler@ib.cegos.fr>
2026-08-17 13:27:10 +0200

    test recup infos git

--- hooks/tools.py
+++ hooks/tools.py
@@ -3,6 +3,9 @@ import re
 import yaml
 import subprocess
 from pathlib import Path
+racine_git = Path.cwd()
+print("CWD =", racine_git)
+print( "RELATIF =", Path(dossier_stage).relative_to(racine_git) )
 
 REGEX_EXERCICE = re.compile( r"a(\d+)e(\d+)\.md$", re.IGNORECASE )
 REGEX_SOMMAIRE = re.compile( r"\{\{\s*sommaire\s*\(\s*\)\s*\}\}", re.IGNORECASE )
@@ -168,8 +171,9 @@ def est_page_print(page):
     return page.file.src_uri.endswith("/print.md")
 
 def recuperer_infos_git(dossier_stage):
+    chemin_git = str( Path(dossier_stage).relative_to(Path.cwd()))
     try:
-        resultat = subprocess.run( ["git","log","-1","--format=%h|%an|%ad","--date=format:%d/%m/%Y",'--',str(dossier_stage)],capture_output=True,text=True,check=True)
+        resultat = subprocess.run( ["git","log","-1","--format=%h|%an|%ad","--date=format:%d/%m/%Y",'--',chemin_git],capture_output=True,text=True,check=True)
         git_version, auteur, date_edition = (resultat.stdout.strip().split("|", 2))
         print("CHEMIN =", dossier_stage)
         print("RESULTAT =", resultat.stdout)
