a2f86bf96b69092fe3187c775fbdcbacb4c0a7dc
renaudwangler <renaud.wangler@ib.cegos.fr>
2026-09-25 11:31:57 +0200

    Correction téléchargements

--- docs/msms030/ressources/problemUsers.ps1
+++ docs/msms030/ressources/problemUsers.ps1
@@ -4,19 +4,16 @@ If ($upn.UserPrincipalName -notlike '*@@*'){
 	$arrupn = $upn.UserPrincipalName -split '@'
 	$newUPN = $arrUPN[0] + '@@' + $arrUPN[1]
 	Set-ADUser Klemen -UserPrincipalName $newUPN
-	Write-Host 'Klemen modifié.'}
-Else { Write-Host 'Klemen était déjà à jour.'}
+	Write-Host 'Traintement de Klemen.'}
 
 # mise à jour de l'attribut emailAddress de 'Lara'
 $emailLara = Get-ADUser Lara -Properties emailAddress | Select emailAddress
 If ($emailLara.emailAddress -notlike 'Lara@adatum.com'){
 	Set-ADUser Lara -emailAddress Lara@adatum.com
-	Write-Host 'Lara mise à jour.'}
-Else { Write-Host 'Lara était déjà à jour.' }
+	Write-Host 'Traitement de Lara.'}
 
 # mise à jour de l'attribut emailAddress de 'Logan'
 $emailLogan = Get-ADUser Logan -Properties emailAddress | Select emailAddress
 If ($emailLogan.emailAddress -notlike 'Lara@adatum.com'){
 	Set-ADUser Logan -emailAddress Lara@adatum.com
-	Write-Host 'Logan modifié.'}
-Else { Write-Host 'Logan était déjà à jour.' }
+	Write-Host 'Traitement de Logan.'}
